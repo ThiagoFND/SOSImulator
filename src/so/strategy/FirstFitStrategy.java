54082bd271843ac43400a146a29761782f688587
ThiagoFND <thiagop4t0d4gu4@gmail.com>
2024-03-15 21:37:47 -0300

    Alteração pra remoção de comentários

--- src/so/strategy/FirstFitStrategy.java
+++ src/so/strategy/FirstFitStrategy.java
@@ -15,16 +15,12 @@ public class FirstFitStrategy implements AllocationStrategy {
 			if (memory[i] == null) {
 				freeSpace++;
 				if (freeSpace == sizeNeeded) {
-					// Encontrou espaço suficiente, retorna o índice inicial do espaço
 					return Optional.of(i - sizeNeeded + 1);
 				}
 			} else {
-				// Se encontrar um bloco ocupado, reseta a contagem de espaço livre
 				freeSpace = 0;
 			}
 		}
-
-		// Se não encontrar espaço suficiente, retorna um Optional vazio
 		return Optional.empty();
 	}
 }
